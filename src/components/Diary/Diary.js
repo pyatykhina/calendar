74d591c635d6f4b21c15102589bd9704d55ee988
Tatiana Pyatykhina <tpyatyhina@mail.ru>
2019-02-26 19:56:35 +0300

    init commit

---
+++ src/components/Diary/Diary.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Diary.css';
+
+class Diary extends Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default Diary;
